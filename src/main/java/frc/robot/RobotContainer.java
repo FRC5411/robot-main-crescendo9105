5cd0224964453fa0669051830178a0b4f919d103
barbute <endman3040@gmail.com>
2024-01-24 10:29:41 -0600

    Initialize Intake in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,17 +21,31 @@ import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOSparkMax;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.IntakeIO;
+import frc.robot.subsystems.intake.IntakeIOSim;
+import frc.robot.subsystems.intake.IntakeIOSparkMax;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class RobotContainer {
   private Drive robotDrive;
+  private Intake robotIntake;
 
   private CommandXboxController pilotController = new CommandXboxController(0);
-  // private CommandPS4Controller pilotController = new CommandPS4Controller(0);
 
-  private final LoggedDashboardChooser<Command> AUTO_CHOOSER;
+  private final LoggedDashboardChooser<Command> AUTO_CHOOSER =
+      new LoggedDashboardChooser<>("Autonomous Selector", AutoBuilder.buildAutoChooser());
 
   public RobotContainer() {
+    initializeSubsystems();
+
+    configureAutonomous();
+
+    configureButtonBindings();
+  }
+
+  /** Instantiate subsystems */
+  private void initializeSubsystems() {
     switch (Constants.currentMode) {
       case REAL:
         robotDrive =
@@ -41,6 +55,7 @@ public class RobotContainer {
                 new ModuleIOSparkMax(2),
                 new ModuleIOSparkMax(3),
                 new GyroIOPigeon2(false));
+        robotIntake = new Intake(new IntakeIOSparkMax());
         break;
       case SIM:
         robotDrive =
@@ -50,6 +65,7 @@ public class RobotContainer {
                 new ModuleIOSim(2),
                 new ModuleIOSim(3),
                 new GyroIO() {});
+        robotIntake = new Intake(new IntakeIOSim());
         break;
       default:
         robotDrive =
@@ -59,44 +75,25 @@ public class RobotContainer {
                 new ModuleIO() {},
                 new ModuleIO() {},
                 new GyroIO() {});
+        robotIntake = new Intake(new IntakeIO() {});
         break;
     }
-
-    NamedCommands.registerCommand(
-        "Print Pose", Commands.print("Pose: " + robotDrive.getPosition()));
-
-    AUTO_CHOOSER =
-        new LoggedDashboardChooser<>("Autonomous Selector", AutoBuilder.buildAutoChooser());
-
-    AUTO_CHOOSER.addDefaultOption("Print Hello", new PrintCommand("Hello"));
-    AUTO_CHOOSER.addOption(
-        "12V Drive Test",
-        Commands.runOnce(
-                () -> {
-                  robotDrive.setDriveVolts(12.0);
-                },
-                robotDrive)
-            .repeatedly()
-            .withTimeout(10.0));
-    AUTO_CHOOSER.addOption(
-        "6V Drive Test",
-        Commands.runOnce(
-                () -> {
-                  robotDrive.setDriveVolts(6.0);
-                },
-                robotDrive)
-            .repeatedly()
-            .withTimeout(10.0));
-    AUTO_CHOOSER.addOption(
-        "Run Auton",
-        Commands.runOnce(() -> robotDrive.setPose(new Pose2d()))
-            .andThen(Commands.run(() -> robotDrive.setDriveVolts(2), robotDrive).withTimeout(2)));
-    // AUTO_CHOOSER.addOption("Straight 1 Meter", getAutonomousCommand());
-
-    configureButtonBindings();
   }
 
+  /** Register commands with PathPlanner and add default autos to chooser */
+  private void configureAutonomous() {
+    // Register commands with PathPlanner's AutoBuilder so it can call them
+    NamedCommands.registerCommand(
+        "Print Pose", Commands.print("Pose: " + robotDrive.getPosition()));
+    NamedCommands.registerCommand(
+        "Intake", Commands.run(() -> robotIntake.setVelocity(1), robotIntake));
+
+    AUTO_CHOOSER.addDefaultOption("Print Hello", new PrintCommand("Hello"));
+  }
+
+  /** Configure controllers */
   private void configureButtonBindings() {
+    /* Drive with joysticks */
     robotDrive.setDefaultCommand(
         SwerveCommands.swerveDrive(
             // FIXME Figure out why joysticks are being goofy
@@ -104,7 +101,7 @@ public class RobotContainer {
             () -> pilotController.getLeftY(),
             () -> pilotController.getLeftX(),
             () -> pilotController.getRightX()));
-    // Reset heading
+    /* Reset drive heading | Debugging */
     pilotController
         .y()
         .onTrue(
@@ -115,14 +112,11 @@ public class RobotContainer {
                                 robotDrive.getPosition().getTranslation(), new Rotation2d())),
                     robotDrive)
                 .ignoringDisable(true)); // Reset even when disabled
+    /* Reset drive pose | Debugging */
     pilotController.a().onTrue(Commands.runOnce(robotDrive::resetPose, robotDrive));
-    // pilotController
-    //     .a()
-    //     .whileTrue(
-    //         AutoBuilder.pathfindToPose(
-    //             robotDrive.getPathFinderSetpoint(), robotDrive.getPathConstraints()));
   }
 
+  /** Returns the selected autonomous */
   public Command getAutonomousCommand() {
     return AUTO_CHOOSER.get();
   }
