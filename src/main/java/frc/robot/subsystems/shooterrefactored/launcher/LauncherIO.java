f167ecaf7e6d6952713df2f92405d848b49800bf
barbute <endman3040@gmail.com>
2024-02-21 11:36:51 -0600

    LauncherIO boilerplate

--- src/main/java/frc/robot/subsystems/shooterrefactored/launcher/LauncherIO.java
+++ src/main/java/frc/robot/subsystems/shooterrefactored/launcher/LauncherIO.java
@@ -4,5 +4,30 @@
 
 package frc.robot.subsystems.shooterrefactored.launcher;
 
-/** Add your docs here. */
-public class LauncherIO {}
+import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+
+/** Hardware interface for the launcher */
+public interface LauncherIO {
+  /** Shooter subsystem launcher sensor data */
+  @AutoLog
+  public static class LauncherIOInputs {
+    public Rotation2d topFlywheelPosition = new Rotation2d();
+    public double topFlywheelVelocityRPM = 0.0;
+    public double topFlywheelappliedVolts = 0.0;
+    public double[] topFlywheelAppliedCurrentAmps = new double[] {0.0};
+    public double[] topFlywheelTemperatureCelsius = new double[] {0.0};
+
+    public Rotation2d bottomFlywheelPosition = new Rotation2d();
+    public double bottomFlywheelVelocityRPM = 0.0;
+    public double bottomFlywheelAppliedVolts = 0.0;
+    public double[] bottomFlywheelAppliedCurrentAmps = new double[] {0.0};
+    public double[] bottomFlywheelTemperatureCelsius = new double[] {0.0};
+  }
+
+  /** Update the inputs from the sensors */
+  public default void updateInputs(LauncherIOInputs inputs) {}
+
+  /** Set the voltage of the launcher motors */
+  public default void setVolts(double topVolts, double bottomVolts) {}
+}
