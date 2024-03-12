fb13b562662a86c89e3d703e20aa2316b312ad1c
Taha Nil <taha.nilfo@gmail.com>
2024-03-12 01:34:01 -0500

    Created base LED logic code with delays

--- src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -4,15 +4,13 @@
 
 package frc.robot.subsystems.leds;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.leds.LEDConstants.Configs;
+import java.util.ArrayList;
 
 public class LEDSubsystem extends SubsystemBase {
   private AddressableLED led;
@@ -21,10 +19,16 @@ public class LEDSubsystem extends SubsystemBase {
   private ArrayList<Runnable> queuedTasks;
   private double taskTargetTimestamp;
   private double taskDelaySec;
-  
+
   private ArrayList<Runnable> queuedPatterns;
   private double patternTargetTimestamp;
   private double patternDelaySec;
+
+  private boolean shouldAnimate;
+  private double animateTargetTimestamp;
+  private double animateDelaySec;
+
+  private boolean tasksJustRanOut;
 
   public LEDSubsystem() {
     led = new AddressableLED(Configs.PWM_PORT);
@@ -34,25 +38,33 @@ public class LEDSubsystem extends SubsystemBase {
     queuedTasks = new ArrayList<Runnable>();
     taskTargetTimestamp = Timer.getFPGATimestamp();
     taskDelaySec = 0.1;
-    
+
     queuedPatterns = new ArrayList<Runnable>();
     patternTargetTimestamp = Timer.getFPGATimestamp();
     patternDelaySec = 0.0;
+
+    animateTargetTimestamp = Timer.getFPGATimestamp();
+    animateDelaySec = 1;
+    shouldAnimate = false;
+
+    tasksJustRanOut = false;
   }
 
-  
-  private void setSolidBlue() {
+  public void setSolidBlue() {
+    setSolidHSV(228/2, 100, 100);
   }
-  
-  private void setSolidBlueGradient() {
-    
+
+  private void setSolidHSV(int h, int s, int v) {
+    shouldAnimate = false;
+    queuedPatterns.add(() -> {
+      taskDelaySec = 0.2;
+      for(int i = 0; i < ledBuffer.getLength(); i++) {
+        final int index = i;
+        queuedTasks.add(() -> ledBuffer.setHSV(index, h, s, v));
+      }
+    });
   }
-  
-  private void clearAnimations() {
-    queuedPatterns.clear();
-    queuedTasks.clear();
-  }
-  
+
   private void setBuffer() {
     led.setData(ledBuffer);
     led.start();
@@ -60,17 +72,34 @@ public class LEDSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    if(queuedTasks.size() == 0){
-      if(queuedPatterns.size() == 0) return;
-      
-      queuedPatterns.
+    if (shouldAnimate) {
+      if(Timer.getFPGATimestamp() >= animateTargetTimestamp) {
+        // Animate a single frameshift.
+        Color lastLED = ledBuffer.getLED(ledBuffer.getLength() - 1);
+        for (int i = ledBuffer.getLength() - 1; i > 0; i--)
+          ledBuffer.setLED(i, ledBuffer.getLED(i - 1));
+        ledBuffer.setLED(0, lastLED);
 
-      queuedPatterns.remove(0).run();
-    }
+        setBuffer();
+        animateTargetTimestamp = Timer.getFPGATimestamp() + animateDelaySec;
+      }
+    } else {
+      if (queuedTasks.size() == 0) {
+        if (queuedPatterns.size() > 0) {
+          if (!tasksJustRanOut) {
+            patternTargetTimestamp = Timer.getFPGATimestamp() + patternDelaySec;
+            tasksJustRanOut = true;
+          }
 
-    if(ledTasks.size() >= 1 && Timer.getFPGATimestamp() >= targetTimestamp) {
-      ledTasks.remove(0).run();
-      targetTimestamp = Timer.getFPGATimestamp() + updateDelay;
+          if (Timer.getFPGATimestamp() >= patternTargetTimestamp) {
+            queuedPatterns.remove(0).run(); // The pattern should populate the tasks arraylist
+            tasksJustRanOut = false;
+          }
+        }
+      } else if (Timer.getFPGATimestamp() >= taskTargetTimestamp) {
+          queuedTasks.remove(0).run();
+          taskTargetTimestamp = Timer.getFPGATimestamp() + taskDelaySec;
+      }
     }
   }
 }
